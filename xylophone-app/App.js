7ed0dd1a2945e904b7643ca592b327e2345f8c6d
tony107 <tony@monkey-monk.fr>
2020-04-30 15:55:26 +0200

    Add styles, sounds locations, buttons

--- xylophone-app/App.js
+++ xylophone-app/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {
+  NoteOne,
+  NoteTwo,
+  NoteThree,
+  NoteFour,
+  NoteFive,
+  NoteSix,
+  NoteSeven
+} from './contants/Colors';
+import { Audio } from 'expo-av';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends React.Component {
+  render () {
+    const xyloSounds = {
+      one: require('./assets/note1.wav'),
+      two: require('./assets/note2.wav'),
+      three: require('./assets/note3.wav'),
+      four: require('./assets/note4.wav'),
+      five: require('./assets/note5.wav'),
+      six: require('./assets/note6.wav'),
+      seven: require('./assets/note7.wav')
+    }
+    
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: NoteOne }]}
+            onPress={() => this.handlePlaySound('one')}
+          >
+            <Text style={styles.buttonText}>Note 1</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -15,5 +44,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
   },
+  buttonContainer: {
+    flex: 1,
+    width: 250,
+  },
+  button: {
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
+
 });
+
+export default App;
